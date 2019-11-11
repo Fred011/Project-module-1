d3dd73d2cfaebf17a00e5360c92337d9adf978be
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-11-11 17:39:19 +0100

    bullet.js changed to es6

--- src/bullet.js
+++ src/bullet.js
@@ -1,69 +1,72 @@
 'use strict'
 
-function Bullet (canvas, playerX, playerY, playerDirection) {
+class Bullet {
+  constructor (canvas, playerX, playerY, playerDirection) {
 
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
-    this.size = 25 ;
-    this.direction = playerDirection;
-    this.speed = 10;
     this.x = playerX + 17;
     this.y = playerY + 17;
+    this.direction = playerDirection;
+    this.speed = 10;
+    this.size = 25;
 
-};
+  };
 
-Bullet.prototype.updatePosition = function () {
+  updatePosition() {
+
     if(this.direction === 'left') {
-        this.x = this.x -= this.speed;
-      }
-  
-      else if (this.direction === 'right') {
-        this.x = this.x += this.speed;
-      }
-  
-      else if (this.direction === 'up') {
-        this.y = this.y -= this.speed;
-      }
-  
-      else if (this.direction === 'down') {
-        this.y = this.y += this.speed;
-      };
-};
+      this.x = this.x -= this.speed;
+    }
 
+    else if (this.direction === 'right') {
+      this.x = this.x += this.speed;
+    }
 
-Bullet.prototype.draw = function () {
+    else if (this.direction === 'up') {
+      this.y = this.y -= this.speed;
+    }
 
+    else if (this.direction === 'down') {
+      this.y = this.y += this.speed;
+    }
+
+  };
+
+  isInsideScreen() {
+    return this.y + this.size / 2 > 0;
+  };
+
+  draw() {
 
     if (this.direction === 'up') {
+    
+      var image = new Image;
+      image.src = './images/laserup.png';
       
-        var image = new Image;
-        image.src = './images/laserup.png';
-        
-      }
-  
-      else if (this.direction === 'left') {
-  
-        var image = new Image;
-        image.src = './images/laserleft.png';
-        
-      }
-      else if (this.direction === 'right') {
-  
-        var image = new Image;
-        image.src = './images/laserright.png';
-        
-      }
-      else if (this.direction === 'down') {
-  
-        var image = new Image;
-        image.src = './images/laserdown.png';
-        
-      }
+    }
+    
+    else if (this.direction === 'left') {
+      
+      var image = new Image;
+      image.src = './images/laserleft.png';
+      
+    }
+    else if (this.direction === 'right') {
+      
+      var image = new Image;
+      image.src = './images/laserright.png';
+      
+    }
+    else if (this.direction === 'down') {
+      
+      var image = new Image;
+      image.src = './images/laserdown.png';
+      
+    }
+    
+    this.ctx.drawImage(image, this.x, this.y, this.size, this.size);
 
-    this.ctx.drawImage(image, this.x, this.y, this.size, this.size)
-}  
-
-Bullet.prototype.isInsideScreen = function () {
-
-    return this.y + this.size / 2 > 0;
-}
+  };
+  
+};
