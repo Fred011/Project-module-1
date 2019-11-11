4ba174d057773be0ac4b88ecda4088e84f144f07
Fred011 <54903136+Fred011@users.noreply.github.com>
2019-11-11 18:43:20 +0100

    boss.js refactored es6

--- src/boss.js
+++ src/boss.js
@@ -1,6 +1,7 @@
 'use strict'
 
-function Boss (canvas, randomX, lives , speed) {
+class Boss {
+    constructor (canvas, randomX, lives , speed) {
 
     this.canvas = canvas;
     this.ctx = this.canvas.getContext('2d');
@@ -11,91 +12,69 @@ function Boss (canvas, randomX, lives , speed) {
     this.x = randomX;
     this.lives = lives;
 
-}; 
+    };
 
-Boss.prototype.removeLife = function () {
+    // REMOVE A LIFE FROM PLAYER
+    removeLife() {
 
-    this.lives -= 1;
-}
+        this.lives -= 1;
 
-Boss.prototype.updatePosition = function () {
+    };
 
-    this.y = this.y += this.speed;
+    // UPDATES THE POSITION OF THE BOSS
+    updatePosition() {
 
-};
+        this.y = this.y += this.speed;
 
-Boss.prototype.isInsideTheScreen = function () {
+    };
 
-    return this.y + (this.size / 2) < this.canvas.height;
+    // CHECKS IF THE BOSS IS STILL IN THE SCREEN
+    isInsideTheScreen() {
 
-};
+        return this.y + (this.size / 2) < this.canvas.height;
 
-Boss.prototype.bossTouched = function (bullet) {
+    };
 
-    var bossLeft = this.x;
-    var bossRight = this.x + this.size;
-    var bossTop = this.y;
-    var bossBottom = this.y + this.size;
-  
-    var bulletLeft = bullet.x;
-    var bulletRight = bullet.x + bullet.size;
-    var bulletTop = bullet.y;
-    var bulletBottom = bullet.y + bullet.size;
-  
-    // Check if the bullet intersects any of the boss's sides
-    var crossLeft = bulletLeft <= bossRight && bulletLeft >= bossLeft;
-      
-    var crossRight = bulletRight >= bossLeft && bulletRight <= bossRight;
+    // CHECKS THE COLLISIONS WITH BULLETS
+    bossTouched(bullet) {
+
+        var bossLeft = this.x;
+        var bossRight = this.x + this.size;
+        var bossTop = this.y;
+        var bossBottom = this.y + this.size;
     
-    var crossBottom = bulletBottom >= bossTop && bulletBottom <= bossBottom;
+        var bulletLeft = bullet.x;
+        var bulletRight = bullet.x + bullet.size;
+        var bulletTop = bullet.y;
+        var bulletBottom = bullet.y + bullet.size;
     
-    var crossTop = bulletTop <= bossBottom && bulletTop >= bossTop;
+        // Check if the bullet intersects any of the boss's sides
+        var crossLeft = bulletLeft <= bossRight && bulletLeft >= bossLeft;
+        
+        var crossRight = bulletRight >= bossLeft && bulletRight <= bossRight;
+        
+        var crossBottom = bulletBottom >= bossTop && bulletBottom <= bossBottom;
+        
+        var crossTop = bulletTop <= bossBottom && bulletTop >= bossTop;
 
-    var crossInside = bulletLeft <= bossLeft && bulletRight >= bossRight;
-  
-    if ((crossInside || crossLeft || crossRight) && (crossTop || crossBottom)) {
-      return true;
-    }
-    return false;
-
-}
-
-Boss.prototype.bottomScreenCollision = function (screen) {
-
-    // var bossLeft = this.x;
-    // var bossRight = this.x + this.size;
-    // var bossTop = this.y;
-    var bossBottom = this.y + this.size;
-  
-    // var screenLeft = screen.x;
-    // var screenRight = screen.x + screen.size;
-    // var screenTop = screen.y;
-    var screenBottom = screen.y + screen.size;
-  
-    // Check if the screen intersects any of the boss's sides
-    // var crossLeft = screenLeft <= bossRight && screenLeft >= bossLeft;
-      
-    // var crossRight = screenRight >= bossLeft && screenRight <= bossRight;
+        var crossInside = bulletLeft <= bossLeft && bulletRight >= bossRight;
     
-    var crossBottom = bossBottom > screenBottom;
-    
-    // var crossTop = screenTop <= bossBottom && screenTop >= bossTop;
+        if ((crossInside || crossLeft || crossRight) && (crossTop || crossBottom)) {
 
-    // var crossInside = screenLeft <= bossLeft && screenRight >= bossRight;
-  
-    if (crossBottom) {
-      return true;
-    }
-    return false;
+        return true;
 
-}
+        }
+        return false;
+    };
 
+    // DRAW THE BOSS
+     draw() {
 
-Boss.prototype.draw = function () {
+        var image = new Image;
+        image.src = './images/boss3.png';
 
-    var image = new Image;
-    image.src = './images/boss3.png';
+        this.ctx.drawImage(image, this.x, this.y, this.size, this.size)
 
-    this.ctx.drawImage(image, this.x, this.y, this.size, this.size)
+        };
 
 };
